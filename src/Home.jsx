0f877327e441a24eac1edc0626d34f36aa808b70
steve-rops <somebiss@gmail.com>
2024-03-21 19:20:46 +0200

    stable- link btn added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <Link to="app">app</Link>
+    </div>
+  );
 };
 
 export default Home;
