987bab941122e7267e516ee3b95a6b2a4112c42d
steve-rops <somebiss@gmail.com>
2024-03-23 22:01:55 +0200

    basic styling

--- src/features/Drawer/MyDrawer.jsx
+++ src/features/Drawer/MyDrawer.jsx
@@ -8,12 +8,11 @@ import JobsSkeleton from "../Jobs/JobsSkeleton";
 const MyDrawer = () => {
   const { isOpen, dispatch } = useDrawerState();
 
-  // const { isLoading } = useQuery({
-  //   queryKey: ["jobs"],
-  //   queryFn: getDefaultJobs,
-  // });
-
-  const isLoading = true;
+  const { isLoading } = useQuery({
+    queryKey: ["jobs"],
+    queryFn: getDefaultJobs,
+  });
+  console.log(isLoading);
 
   return (
     <Drawer.Root
@@ -27,9 +26,9 @@ const MyDrawer = () => {
       <Drawer.Portal>
         <Drawer.Trigger></Drawer.Trigger>
         <Drawer.Overlay className="" />
-        <Drawer.Content className="bg-white flex flex-col fixed bottom-0 left-0 right-0 max-h-[93%] rounded-t-[10px]">
+        <Drawer.Content className="bg-white flex flex-col fixed bottom-0 left-0 right-0 max-h-[94%] rounded-t-[10px]">
           <div className="h-4 w-8 bg-foreground my-2 m-auto rounded-md" />
-          <div className="  mx-2 flex flex-col overflow-auto p-4 rounded-t-[10px]">
+          <div className="flex flex-col gap-2  align-middle overflow-auto p-2 rounded-t-[10px]">
             {isLoading ? <JobsSkeleton /> : <JobsListing />}
           </div>
         </Drawer.Content>
--- src/features/Jobs/SingleJob.jsx
+++ src/features/Jobs/SingleJob.jsx
@@ -1,7 +1,9 @@
 const SingleJob = ({ job }) => {
   return (
-    <div>
-      <h1>{job.description}</h1>
+    <div className="border-l-4 border-primary rounded-md">
+      <div className="p-1 bg-secondary ">
+        <h3 className="text-md w-[50%] text-pretty">{job.title}</h3>
+      </div>
     </div>
   );
 };
