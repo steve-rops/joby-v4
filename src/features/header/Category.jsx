a10531973d44973386bc488bf7f373075013651d
steve-rops <somebiss@gmail.com>
2024-03-27 18:43:31 +0200

    small changes on Header -stable

--- src/features/header/Category.jsx
+++ src/features/header/Category.jsx
@@ -1,18 +1,25 @@
 const Category = ({ cat, activeID, setActiveID }) => {
+  const isActive = activeID === cat.id;
   return (
     <div
       onClick={() => setActiveID(cat.id)}
       className={`flex flex-col items-center transition-all ${
-        activeID === cat.id ? "border-b border-primary" : ""
+        isActive ? "border-b border-primary" : ""
       }  `}
       key={cat.label}
     >
       <span
-        className={`text-3xl text-primary${activeID === cat.id ? "" : "/60"}`}
+        className={`text-3xl ${isActive ? "text-primary" : "text-primary/60"}`}
       >
         {cat.logo}
       </span>
-      <h4 className="text-balance text-xs  text-center ">{cat.label}</h4>
+      <h4
+        className={`text-balance text-xs  text-center ${
+          isActive ? "text-slate-900" : "text-slate-600"
+        }`}
+      >
+        {cat.label}
+      </h4>
     </div>
   );
 };
