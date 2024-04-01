2a044a6c8d08906d4ae9c404b0904823f1b2b5a0
steve-rops <somebiss@gmail.com>
2024-04-01 17:02:25 +0300

    test#1 map -stable

--- src/features/map/Map.jsx
+++ src/features/map/Map.jsx
@@ -1,16 +1,13 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useSelector } from "react-redux";
 
 const Map = () => {
-  const { height: topBarHeight } = useSelector((store) => store.height);
-
   return (
     <div className="h-screen w-full fixed bottom-0">
       <MapContainer
         center={[51.505, -0.09]}
         zoom={13}
         scrollWheelZoom={false}
-        className={`h-[calc(100%-${topBarHeight}px)] absolute bottom-0 w-full`}
+        className={`h-[calc(100%-174px)] absolute bottom-0 w-full`}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
