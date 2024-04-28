b23026f6cf1af0f9d020ba11d840dabfa6ebeadf
steve-rops <somebiss@gmail.com>
2024-04-28 21:39:45 +0300

    humburger mobile menu added -stable

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,35 +1,15 @@
-import { NavLink, Navigate } from "react-router-dom";
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-} from "@clerk/clerk-react";
-import { Button } from "./components/ui/button";
+import HamburgerMenu from "./features/hamburger/HamburgerMenu";
 
 const Home = () => {
   return (
     <div className="h-svh w-full">
-      <header className="p-2 flex items-center justify-between">
+      <header className="p-2 flex items-center justify-between shadow-md">
         <h1 className="text-2xl">
           Joby <span className="text-xs text-primary ">v4</span>
         </h1>
 
         <nav className="space-x-2">
-          <NavLink className="text-sm" to="/about">
-            About
-          </NavLink>
-          <NavLink className="text-sm" to="/employers">
-            Employers
-          </NavLink>
-          <SignedOut>
-            <Button size="sm">
-              <SignInButton />
-            </Button>
-            <Button size="sm" variant="outline">
-              <SignUpButton />
-            </Button>
-          </SignedOut>
+          <HamburgerMenu />
         </nav>
       </header>
     </div>
---
+++ src/features/hamburger/HamburgerMenu.jsx
@@ -0,0 +1,51 @@
+import { Drawer } from "vaul";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
+import { SignInButton, SignUpButton, SignedOut } from "@clerk/clerk-react";
+import { Button } from "../../components/ui/button";
+
+const HamburgerMenu = () => {
+  return (
+    <div>
+      <Drawer.Root direction="right">
+        <Drawer.Trigger>
+          <span>
+            <GiHamburgerMenu className="text-2xl" />
+          </span>
+        </Drawer.Trigger>
+        <Drawer.Portal>
+          <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+          <Drawer.Content className="bg-white flex flex-col rounded-t-[10px] h-full w-[60%] mt-24 fixed bottom-0 right-0">
+            <div className="p-4 bg-white flex-1 h-full">
+              <div className="max-w-md mx-auto">
+                <Drawer.Title className="font-medium mb-4">
+                  <span className="text-primary">pages</span>
+                </Drawer.Title>
+              </div>
+
+              <nav className="flex flex-col mb-2 text-xl">
+                <NavLink className="" to="/about">
+                  About
+                </NavLink>
+                <NavLink className="" to="/employers">
+                  Employers
+                </NavLink>
+              </nav>
+
+              <SignedOut>
+                <Button className="">
+                  <SignInButton />
+                </Button>
+                <Button variant="outline">
+                  <SignUpButton />
+                </Button>
+              </SignedOut>
+            </div>
+          </Drawer.Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
