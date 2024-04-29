dfec2d9053e4c975b262bc739eeba09a9de35850
steve-rops <somebiss@gmail.com>
2024-04-29 14:02:45 +0300

    centered the home page elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Button } from "../components/ui/button";
 
 const Home = () => {
   return (
-    <div className="h-svh w-full">
+    <div className="h-svh w-full flex flex-col">
       <header className="p-4 flex items-center justify-between shadow-md">
         <h1 className="text-5xl">
           Joby <span className="text-xs text-primary ">v4</span>
@@ -15,7 +15,7 @@ const Home = () => {
         </nav>
       </header>
 
-      <section className="p-10 flex flex-col items-center gap-4 ">
+      <section className="p-10 flex-1 justify-center flex flex-col items-center gap-4 ">
         <h1 className="text-4xl">Find your dream Job.</h1>
 
         <div className="flex flex-col items-center gap-2">
