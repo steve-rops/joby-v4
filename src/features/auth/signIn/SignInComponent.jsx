fd80b572909981e4e255aecb3855e2a5b7cf748b
steve-rops <somebiss@gmail.com>
2024-04-30 12:43:51 +0300

    route testing #4

--- src/features/auth/signIn/SignInComponent.jsx
+++ src/features/auth/signIn/SignInComponent.jsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react";
 const SignInComponent = () => {
   return (
     <div className="h-svh w-full grid place-items-center bg-primary/35">
-      <SignIn forceRedirectUrl="app" />
+      <SignIn forceRedirectUrl="app" fallbackRedirectUrl="/" />
     </div>
   );
 };
--- src/features/auth/signup/SignUpComponent.jsx
+++ src/features/auth/signup/SignUpComponent.jsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/clerk-react";
 const SignUpComponent = () => {
   return (
     <div className="h-svh w-full grid place-items-center bg-primary/35">
-      <SignUp forceRedirectUrl="app" />
+      <SignUp forceRedirectUrl="/app" fallbackRedirectUrl="/" />
     </div>
   );
 };
