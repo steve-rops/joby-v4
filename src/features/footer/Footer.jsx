e037c4432a3c1a6a6f8b1c7929cf211d670c52ad
steve-rops <somebiss@gmail.com>
2024-05-03 19:47:20 +0300

    added liked option

--- src/features/footer/Footer.jsx
+++ src/features/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { UserButton } from "@clerk/clerk-react";
+import { FaHeart } from "react-icons/fa6";
 
 const Footer = ({ display }) => {
   return (
@@ -7,7 +8,11 @@ const Footer = ({ display }) => {
       className="fixed bottom-0 w-full bg-secondary shadow-inner border-t border-gray-500 rounded-t-xl"
     >
       <div className=" flex items-center justify-center p-3 gap-3">
-        <div className="">test</div>
+        <span className="flex items-center">
+          <p>liked</p>
+          <FaHeart className="text-red-600" />
+        </span>
+
         <span className="">
           <UserButton />
         </span>
